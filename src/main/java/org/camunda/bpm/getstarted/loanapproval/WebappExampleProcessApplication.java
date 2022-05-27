abd4a051a2cdbae4a596ae49bff14bb3a0ae9409
LuanBraga <luanbraga.lb@gmail.com>
2022-05-27 16:24:09 -0300

    first commit

---
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/WebappExampleProcessApplication.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappExampleProcessApplication {
+    public static void main(String... args) {
+        SpringApplication.run(WebappExampleProcessApplication.class, args);
+    }
+}
